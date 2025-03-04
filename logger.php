229c4548f183acd0314e8eefbd9b6d7d280e0242
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2025-03-04 16:33:47 +0330

    Update logger.php

--- logger.php
+++ logger.php
@@ -1,11 +1,30 @@
 <?php
-// خواندن داده‌های ورودی از stdin
-$sip_data = file_get_contents("php://stdin");
 
-if (!empty($sip_data)) {
-    file_put_contents("sip_logs.txt", $sip_data . PHP_EOL, FILE_APPEND);
-    echo "✅ Data saved to sip_logs.txt\n";
-} else {
-    echo "⚠️ No data received\n";
+
+require __DIR__ . "/vendor/autoload.php";
+
+$text = file_get_contents("php://stdin");
+
+if (empty($sip_data)) {
+    echo "There is no data !!";
+    return;
 }
+
+try {
+    // تجزیه پیام SIP
+    $response = \RTCKit\SIP\Message::parse($text);
+
+    // تبدیل به JSON
+    $jsonData = json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+    // ذخیره در فایل
+    file_put_contents('sip_response.json', $jsonData);
+
+    echo "JSON ذخیره شد: sip_response.json\n";
+
+} catch (Exception $e) {
+    echo "خطا در پردازش SIP: " . $e->getMessage();
+}
+
+
 ?>
