bd7cc0341708e489dc406e847213ce47e051b39b
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2025-03-05 18:35:53 +0330

    Update snif2.py

--- snif2.py
+++ snif2.py
@@ -3,10 +3,10 @@ from scapy.all import sniff, UDP
 def packet_callback(pkt):
     if pkt.haslayer(UDP):
         print(f"Packet: {pkt.show()}")  # نمایش تمام لایه‌های پکت
-        if pkt.haslayer(Raw):
-            print("Raw Layer Data:")
-            print(pkt[Raw].load.decode(errors='ignore'))
-        else:
-            print("No Raw Data in packet")
+        # if pkt.haslayer(Raw):
+        #     print("Raw Layer Data:")
+        #     print(pkt[Raw].load.decode(errors='ignore'))
+        # else:
+        #     print("No Raw Data in packet")
 
 sniff(prn=packet_callback, filter="udp port 5060", store=0)
