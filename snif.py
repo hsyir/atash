e87e0cfd2ca653c4c261dcc3090ced70043f126b
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2025-03-05 18:10:25 +0330

    Update snif.py

--- snif.py
+++ snif.py
@@ -34,7 +34,7 @@ def packet_callback(pkt):
 
             # نمایش کل محتوای SIP برای Debug
             print("------ SIP Packet Received ------")
-            #print(raw_data)
+            print(raw_data)
             #print("---------------------------------")
             #print("")
             print("")
